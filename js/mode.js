f03a80d663df290ed40d648b37abfbb7b2f78129
Hanzo080501 <yohanisszogara@gmail.com>
2024-04-01 12:49:59 +0700

    perubahan dikit

--- js/mode.js
+++ js/mode.js
@@ -1,25 +1,18 @@
-const body = document.querySelector("body");
-const elements = document.querySelectorAll(".footer-section, .navbar, .modal-content");
-const defaultMod = body.style.backgroundImage
-const input = document.querySelector(".form-check-input");
+const btn = document.body;
 
 let dark = () => {
-  const btn = document.body
-  btn.classList.toggle("dark")
-  if (body.classList.contains("dark")) {
-    body.style.cssText = `background-image : none;`
-    body.style = `background-color : none;`
-    input.style.backgroundColor = "#292929"
-    elements.forEach(element => {
-        element.style.backgroundColor = '#565656';
-    });
+  btn.classList.toggle('dark');
+  if (btn.classList.contains('dark')) {
+    localStorage.setItem('modeGelap', 'true');
   } else {
-    body.style.backgroundImage = defaultMod;
-    input.style.backgroundColor = "";
-    elements.forEach(element => {
-        element.style.backgroundColor = 'rgba(217, 217, 217, 1)';
-    });
-    modal.style.backgroundColor = "none;"
-    
+    localStorage.setItem('modeGelap', 'false');
   }
-}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('modeGelap') === 'true') {
+    btn.classList.add('dark');
+  } else {
+    btn.classList.remove('dark');
+  }
+});
